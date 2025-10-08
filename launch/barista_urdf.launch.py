54a209f7768df5ce653bec75b42949236d664130
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-10-08 07:14:57 +0000

    Add a logic to start gazebo and spawn barista model in launch

--- launch/barista_urdf.launch.py
+++ launch/barista_urdf.launch.py
@@ -2,22 +2,74 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_prefix
+
 
 def generate_launch_description():
 
-    ####### DATA INPUT ##########
     urdf_file = 'barista_robot_model.urdf'
-    #xacro_file = "urdfbot.xacro"
     package_description = "barista_robot_description"
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_barista_robot = get_package_share_directory(package_description)
+    install_dir = get_package_prefix(package_description)
 
-    ####### DATA INPUT END ##########
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_barista_robot, 'models')
+    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+
+    # Position and orientation
+    # [X, Y, Z]
+    position = [0.0, 0.0, 0.2]
+    # [Roll, Pitch, Yaw]
+    orientation = [0.0, 0.0, 0.0]
+    # Base Name or robot
+    entity_name = "barista_robot"
+
+    # Spawn ROBOT Set Gazebo
+    spawn_robot = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        name='spawn_entity',
+        output='screen',
+        arguments=['-entity',
+                   entity_name,
+                   '-x', str(position[0]), '-y', str(position[1]
+                                                     ), '-z', str(position[2]),
+                   '-R', str(orientation[0]), '-P', str(orientation[1]
+                                                        ), '-Y', str(orientation[2]),
+                   '-topic', '/robot_description'
+                   ]
+    )
+
     print("Fetching URDF ==>")
-    robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
+    robot_desc_path = os.path.join(pkg_barista_robot, "urdf", urdf_file)
 
     # Robot State Publisher
-
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -28,8 +80,7 @@ def generate_launch_description():
     )
 
     # RVIZ Configuration
-    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
-
+    rviz_config_dir = os.path.join(pkg_barista_robot, 'rviz', 'urdf_vis.rviz')
 
     rviz_node = Node(
             package='rviz2',
@@ -42,7 +93,13 @@ def generate_launch_description():
 
     # create and return launch description object
     return LaunchDescription(
-        [            
+        [
+            DeclareLaunchArgument(
+                'world', 
+                default_value=[os.path.join(pkg_barista_robot, 'worlds', 'empty.world'), ''], 
+                description='SDF world file'),
+            gazebo,
+            spawn_robot,
             robot_state_publisher_node,
             rviz_node
         ]
